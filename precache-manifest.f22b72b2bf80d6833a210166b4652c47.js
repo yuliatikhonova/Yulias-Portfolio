89c6321883f579ed0e270f3a756745d7a7f39b65
yulia tikhonova <yuliatikhonova@outlook.com>
2020-11-23 23:45:04 -0700

    Updates

---
+++ precache-manifest.f22b72b2bf80d6833a210166b4652c47.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6f0ed334495c95ec92a6d2da90d15b",
+    "url": "/Yulias-Portfolio/index.html"
+  },
+  {
+    "revision": "23afc5fa8ce7640f3dae",
+    "url": "/Yulias-Portfolio/static/css/2.3c1c8673.chunk.css"
+  },
+  {
+    "revision": "a34c25a5b4bd89ac6f16",
+    "url": "/Yulias-Portfolio/static/css/main.d40d3d45.chunk.css"
+  },
+  {
+    "revision": "23afc5fa8ce7640f3dae",
+    "url": "/Yulias-Portfolio/static/js/2.33fb6d52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Yulias-Portfolio/static/js/2.33fb6d52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34c25a5b4bd89ac6f16",
+    "url": "/Yulias-Portfolio/static/js/main.0e44f55a.chunk.js"
+  },
+  {
+    "revision": "9d6e2ebbdf61d085afbb",
+    "url": "/Yulias-Portfolio/static/js/runtime-main.130d66f8.js"
+  },
+  {
+    "revision": "d5fcc8caaa83900a03394b323ee06251",
+    "url": "/Yulias-Portfolio/static/media/Code Quiz.d5fcc8ca.png"
+  },
+  {
+    "revision": "20e486c822ae656661d91f209fffe1dc",
+    "url": "/Yulias-Portfolio/static/media/Lyrics to Go.20e486c8.png"
+  },
+  {
+    "revision": "5fc0098f1e2bd5c14d952a4fc51b0a33",
+    "url": "/Yulias-Portfolio/static/media/Screen Shot 2020-07-02 at 12.50.47 AM.5fc0098f.png"
+  },
+  {
+    "revision": "b21ff62048714d65cf00f5d6af256b28",
+    "url": "/Yulias-Portfolio/static/media/Screen Shot 2020-07-25 at 8.07.05 PM.b21ff620.png"
+  },
+  {
+    "revision": "ddfa96b7ff81abffbfad3dccdfdaf1ca",
+    "url": "/Yulias-Portfolio/static/media/TukTuk.ddfa96b7.png"
+  },
+  {
+    "revision": "e842cae2dd0cd4b63941cfc35e05081a",
+    "url": "/Yulias-Portfolio/static/media/Work Day Scheduler.e842cae2.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Yulias-Portfolio/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Yulias-Portfolio/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Yulias-Portfolio/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Yulias-Portfolio/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Yulias-Portfolio/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Yulias-Portfolio/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Yulias-Portfolio/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Yulias-Portfolio/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Yulias-Portfolio/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Yulias-Portfolio/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Yulias-Portfolio/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Yulias-Portfolio/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Yulias-Portfolio/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Yulias-Portfolio/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Yulias-Portfolio/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
